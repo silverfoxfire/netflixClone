cdec5a4195d2ba63c3f3457a4e0ae7007245edbb
silverfoxfire <232652342+silverfoxfire@users.noreply.github.com>
2025-09-17 13:47:06 -0300

    Added tab switching logic to script

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-console.log("it lives, probably");
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+//Select tab content items
+function selectItem(e){
+	removeBorder();
+	removeShow();
+	//Add border to current tab
+	this.classList.add('tab-border');
+
+	//Get right tab
+	const tabContentItem = document.querySelector(`#${this.id}-content`);
+
+	tabContentItem.classList.add('show');
+}
+
+function removeBorder(){
+	tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow(){
+	tabContentItems.forEach(item => item.classList.remove('show'));
+}
+
+
+// Listen for Tab Click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
